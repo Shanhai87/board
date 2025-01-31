448a1c3938a2682585d715e21a520d19c9e163f8
lpuddy <tenochtitlan2010@yandex.ru>
2025-01-31 11:52:50 +0300

    routes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/board/', // Путь, который соответствует вашему репозиторию на GitHub Pages
-  build: {
-    outDir: 'dist', // где будет собираться проект
-  },
 })
