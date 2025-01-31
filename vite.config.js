4483abad03fa8e3860f4ba53bb070dd6d130bf90
lpuddy <tenochtitlan2010@yandex.ru>
2025-01-31 13:02:15 +0300

    routes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/board/', // Путь, который соответствует вашему репозиторию на GitHub Pages
+  // base: '/board/', // Путь, который соответствует вашему репозиторию на GitHub Pages
   plugins: [react()],
 })
